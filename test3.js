054efaa80ed34d064b85d9ba4b7fe1a35a6d371f
FelipeNadhel <47465864+FelipeNadhelBR@users.noreply.github.com>
2020-01-16 17:09:39 -0300

    Add files via upload

---
+++ test3.js
@@ -0,0 +1,41 @@
+class NitroBlock {
+    //By: Felipe F.
+    getInfo() {
+        return {
+            "id": "Browser",
+            "name": "Browser",
+            "blocks": [{
+                "opcode": "jQuGet",
+                "blockType": "command",
+                "text": "go to url: [myURL]",
+                "arguments": {
+                    "myURL": {
+                        "type": "string",
+                        "defaultValue": "https://google.com.br"
+                    }
+                }
+            }],
+            "menus": {
+                supermath: this._formatMenu(['+', '-', '/', '*', '^', 'sqrt']),
+            }
+        };
+    }
+
+    jQuGet({
+        myURL
+    }) {
+        $.get(myURL, function(data) {
+            window.open[myURL];
+    }
+    _formatMenu(menu) {
+        const m = [];
+        for (let i = 0; i < menu.length; i++) {
+            const obj = {};
+            obj.text = menu[i];
+            obj.value = i.toString();
+            m.push(obj);
+        }
+        return m;
+    }
+}
+Scratch.extensions.register(new Links());
